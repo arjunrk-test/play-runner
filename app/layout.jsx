e9398c64fef6cc4eca6cb2d12660843f4f9e2ff0
Arjun Saravanakumar <arjunrk.testing@gmail.com>
2025-03-13 04:06:19 +0530

    added few components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,23 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], 
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"], 
   variable: "--font-jetbrainsMono" });
 
 export const metadata = {
-  title: "Arjun Saravanakumar",
-  description: "Portfolio by Arjun Saravanakumar",
+  title: "Play Runner",
+  description: "Generates Playwright automations",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
-        {children}
+        <TooltipProvider>
+          {children}
+        </TooltipProvider>
       </body>
     </html>
   );
