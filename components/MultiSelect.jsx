08d65a9ebf84f9910d6e037c4cc604054d49c344
Arjun Saravanakumar <arjunrk.testing@gmail.com>
2025-03-15 08:17:39 +0530

    Added checkbox to the multiselect

--- components/MultiSelect.jsx
+++ components/MultiSelect.jsx
@@ -4,6 +4,7 @@ import {
   XCircle,
   ChevronDown,
   XIcon,
+  CheckIcon,
 } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -207,8 +208,18 @@ export const MultiSelect = React.forwardRef(
             <CommandList>
               <CommandEmpty className="bg-input text-white pl-2">No results found.</CommandEmpty>
               <CommandGroup>
-                <CommandItem  key="all" onSelect={toggleAll} >
-                  (Select All)
+                <CommandItem  key="all" onSelect={toggleAll} className="cursor-pointer" >
+                  <div
+                    className={cn(
+                      "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                      selectedValues.length === options.length
+                        ? "bg-white text-black border-black "
+                        : "border-white [&_svg]:invisible hover:border-black"
+                    )}
+                  >
+                    <CheckIcon className="h-4 w-4" />
+                  </div>
+                  <span>(Select All)</span>
                 </CommandItem>
                 {options.map((option) => {
                   const isSelected = selectedValues.includes(option.value);
@@ -216,8 +227,23 @@ export const MultiSelect = React.forwardRef(
                     <CommandItem
                       key={option.value}
                       onSelect={() => toggleOption(option.value)}
+                      className="cursor-pointer"
                     >
-                      {option.label}
+                      <div
+                        // onSelect={() => toggleOption(option.value)}
+                        className={cn(
+                          "mr-2 flex h-4 w-4 items-center justify-center rounded-sm  border ",
+                          isSelected
+                            ? "bg-white text-black border-black"
+                            : "[&_svg]:invisible border-white hover:border-black"
+                        )}
+                      >
+                        <CheckIcon className="h-4 w-4" />
+                      </div>
+                      {option.icon && (
+                        <option.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+                      )}
+                      <span>{option.label}</span>
                     </CommandItem>
                   );
                 })}
